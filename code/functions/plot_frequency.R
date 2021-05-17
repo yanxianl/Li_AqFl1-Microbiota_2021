dccf692aaacf59296344fc6bac97a496277a05a5
Yanxian Li <yanxianl@nmbu.no>
2021-05-17 13:01:00 +0200

    update license

--- code/functions/plot_frequency.R
+++ code/functions/plot_frequency.R
@@ -1,7 +1,7 @@
 ###############################################################################
 # plot_frequency
 
-# Copyright (c) 2020 Yanxian Li
+# Copyright (c) 2021 Yanxian Li
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
